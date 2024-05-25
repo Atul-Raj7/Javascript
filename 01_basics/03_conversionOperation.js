16cfeb026f4c4003a5f36b8f8e2433c37b878430
Atul-Raj7 <meatulraj7.com>
2024-05-25 20:24:41 +0530

    Part two of conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));  
+// console.log(typeof score);
+// console.log(typeof (score));  
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN Not a Number
@@ -14,7 +14,7 @@ console.log( valueInNumber);
 let isLoggedIn = ""
 
 let boolenIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,5 +23,32 @@ console.log(boolenIsLoggedIn);
 let someNumber = 33
 let stringNumber = String(someNumber) 
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// ***************** Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Atul"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
