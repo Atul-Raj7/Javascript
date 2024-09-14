880219c37bee8f756fb4df5452f9f444a93c12e7
Atul-Raj7 <meatulraj7.com>
2024-09-14 21:40:07 +0530

    Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// let a = 10
+// const b =  20
+// var c = 30
+let a = 300
+if(true){
+    let a = 10
+    const b =  20
+    console.log("INNER: ", a);
+    
+}
+
+// console.log(a);
+// console.log(b);
+console.log(a);
+
+// {} -> If this is used with functions, if-else then it's called Scope. And if used solely it's object
+
